cdf79020c1b73e0692d4ede84b11576cd1297072
Daniel Feldman <cool@gmail.com>
2016-03-18 12:15:30 +0200

    git status

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 18, 2016
+ *  Authors: 1) dormendil ; Dor Mendil ; 200968873
+ * 			 2) danielf1 ; Daniel Feldman ; 302575436
+ */
+
+#include <stdio.h>
+#include "MathIsFun.h"
+
+int main(int argc, char** argv)
+{
+
+
+
+	return 0;
+}
+
